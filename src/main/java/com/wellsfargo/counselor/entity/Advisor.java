6bcf605113a766532d5a4039a99e47d33982e929
Ayush Pathak <138036492+BRUTEee@users.noreply.github.com>
2026-07-13 21:23:45 +0530

    Enhance Advisor entity with clients relationship
    Added OneToMany relationship with Client entity.

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,16 +1,19 @@
 package com.wellsfargo.counselor.entity;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Advisor {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue
     private long advisorId;
 
     @Column(nullable = false)
@@ -27,6 +30,9 @@ public class Advisor {
 
     @Column(nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
 
     protected Advisor() {
 
@@ -82,5 +88,13 @@ public class Advisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
     }
 }
